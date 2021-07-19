a288bfd55e08d9a85b319ddacc1d9f98258c511c
Fengjiale <1351837703@qq.com>
2021-07-19 23:27:47 +0800

    将 svg 代码整理到 icon.vue

--- src/button.vue
+++ src/button.vue
@@ -1,8 +1,24 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button class="g-button" :class="{ [`icon-${iconPosition}`]: true }">
+    <g-icon v-if="icon" :name="icon"></g-icon>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 <script>
-export default {};
+export default {
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: "left",
+      validator(value) {
+        return value === "left" || value === "right";
+      },
+    },
+  },
+};
 </script>
 <style lang="sass">
 .g-button
@@ -12,13 +28,29 @@ export default {};
   border-radius: var(--border-radius)
   border: 1px solid var(--border-color)
   background: var(--button-bg)
-
+  display: inline-flex
+  justify-content: center
+  align-items: center
+  vertical-align: middle
   &:hover
     border-color: var(--border-color-hover)
 
-    &:active
-      background-color: var(--button-active-bg)
+  &:active
+    background-color: var(--button-active-bg)
 
-    &:focus
-      outline: none
+  &:focus
+    outline: none
+
+  >.content
+    order: 2
+  >.icon
+    order: 1
+    margin-right: .3em
+  &.icon-right
+  >.content
+    order: 1
+    >.icon
+      order: 2
+      margin-right: 0
+      margin-left: .3em
 </style>
