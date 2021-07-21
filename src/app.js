e2e2417ac78dab7b54bf4d1a220e21461351bbe2
Fengjiale <1351837703@qq.com>
2021-07-21 21:37:08 +0800

    创建 button-grop

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
+import ButtonGrop from './button-grop'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
+Vue.component('g-button-grop', ButtonGrop)
 
 new Vue({
     el: '#app',
---
+++ src/button-grop.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="g-button-grop">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss">
+.g-button-grop {
+  display: inline-flex;
+  vertical-align: middle;
+  > .g-button {
+    border-radius: 0;
+    margin-left: -1px;
+    &:first-child {
+      border-top-left-radius: var(--border-radius);
+      border-bottom-left-radius: var(--border-radius);
+    }
+    &:last-child {
+      border-top-right-radius: var(--border-radius);
+      border-bottom-right-radius: var(--border-radius);
+    }
+    &:hover {
+      position: relative;
+      z-index: 1;
+    }
+  }
+}
+</style>
