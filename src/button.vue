96637c003a25bdfcc14fffd8f876c87a13847459
Fengjiale <1351837703@qq.com>
2021-07-21 21:07:29 +0800

    添加 click 事件

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,11 @@
 <template>
-  <button class="g-button" :class="{ [`icon-${iconPosition}`]: true }">
-    <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
+  <button
+    class="g-button"
+    :class="{ [`icon-${iconPosition}`]: true }"
+    @click="$emit('click')"
+  >
+    <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
+    <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
     <div class="content">
       <slot></slot>
     </div>
@@ -10,6 +15,10 @@
 export default {
   props: {
     icon: {},
+    loading: {
+      type: Boolean,
+      default: false,
+    },
     iconPosition: {
       type: String,
       default: "left",
@@ -20,37 +29,54 @@ export default {
   },
 };
 </script>
-<style lang="sass">
-.g-button
-  font-size: var(--font-size)
-  height: var(--button-height)
-  padding: 0 1em
-  border-radius: var(--border-radius)
-  border: 1px solid var(--border-color)
-  background: var(--button-bg)
-  display: inline-flex
-  justify-content: center
-  align-items: center
-  vertical-align: middle
-  &:hover
-    border-color: var(--border-color-hover)
-
-  &:active
-    background-color: var(--button-active-bg)
-
-  &:focus
-    outline: none
-
-  >.content
-    order: 2
-  >.icon
-    order: 1
-    margin-right: .3em
-  &.icon-right
-  >.content
-    order: 1
-    >.icon
-      order: 2
-      margin-right: 0
-      margin-left: .3em
+<style lang="scss">
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+.g-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
+  &:active {
+    background-color: var(--button-active-bg);
+  }
+  &:focus {
+    outline: none;
+  }
+  > .content {
+    order: 2;
+  }
+  > .icon {
+    order: 1;
+    margin-right: 0.3em;
+  }
+  &.icon-right {
+    > .content {
+      order: 1;
+    }
+    > .icon {
+      order: 2;
+      margin-right: 0;
+      margin-left: 0.3em;
+    }
+  }
+}
+.loading {
+  animation: spin 1s infinite linear;
+}
 </style>
